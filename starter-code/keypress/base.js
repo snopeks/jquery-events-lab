fde955273ab5f6dc23a91c73cc06ec17a21ea5dc
snopeks <creativesnopeks@gmail.com>
2017-09-28 23:31:35 -0700

    completed homework

--- starter-code/keypress/base.js
+++ starter-code/keypress/base.js
@@ -1,5 +1,29 @@
 console.log("Sanity Check: JS is working!");
 
+var startTime; // undefined
+var endTime; // undefined
+var total = 0;
+
 $(document).ready(function(){
+  $(window).on("keypress", function handleKeypress(event){
+
+    if ( startTime ) {
+      endTime = Date.now();
+      total = (endTime - startTime) / 1000;
+      displayTotal();
+    } else {
+      startTime = Date.now();
+      // when start has been clicked,
+      // make a JavaScript timing function to call
+      // displayTotal every second!
+      window.setInterval(displayTotal, 500);
+    }
+
+  })
 
 })
+
+function displayTotal(){
+  total = (Date.now() - startTime) / 1000;
+  $("#total-time").text( total + " seconds" );
+}
